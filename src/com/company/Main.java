cd29c6bd7615db7e966a06dd0a884191595b33e9
Abrams <dima.abramchik@gmail.com>
2021-05-28 18:16:45 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;            //задача 18
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 0;
+        int[] array = new int[10];
+        for (i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100 + 1);
+            System.out.print(array[i] + " ");
+        }
+        System.out.println(" ");
+        System.out.println("Последний элемент массива = " + array[array.length - 1]);
+    }
+}
