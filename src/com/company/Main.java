342fdbf32a60db50fc1d13294edfc27c98884b96
Abrams <dima.abramchik@gmail.com>
2021-05-29 13:56:18 +0300

    Init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int i = 0;
-        int[] array = new int[10];
-        for (i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() * 100 + 1);
-            System.out.print(array[i] + " ");
-        }
-        System.out.println(" ");
+        int[] array;
+        array = new int[]{9, 8, 6, 2, 4, 5, 7, 6, 8, 99};
         System.out.println("Последний элемент массива = " + array[array.length - 1]);
     }
 }
