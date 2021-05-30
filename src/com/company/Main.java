84d40766275e3b788d856627007fd7e6587eaa5e
Abrams <dima.abramchik@gmail.com>
2021-05-30 04:05:20 +0300

    Init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         int[] array;
         array = new int[]{9, 8, 6, 2, 4, 5, 7, 6, 8, 99};
+        findLastElement(array);
+    }
+    public static void findLastElement(int array[]){
         System.out.println("Последний элемент массива = " + array[array.length - 1]);
     }
 }
